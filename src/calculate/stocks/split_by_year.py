27418db00f261f7b64f141f98b69f99d4844db3a
Amit Drucker <a.druker@f5.com>
2019-08-22 14:49:20 +0300

    prepareDF, add gitignore

--- src/calculate/stocks/split_by_year.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('IVE_tickbidask.txt')
-df.columns = ['Date', 'Time', 'Open', 'Low', 'High', 'Volume']
-for year in range(2009, 2020):
-    yearStr = str(year)
-    print('working on %s' % yearStr)
-    curr_df = df[df['Date'].str.contains(yearStr)]
-    curr_df.to_csv('IVE_tickbidask_' + yearStr + '.csv')
