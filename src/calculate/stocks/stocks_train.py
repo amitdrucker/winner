2e77df8985e8fa1bdafcd08ec63a354100e13989
Amit Drucker <a.druker@f5.com>
2019-08-26 22:14:40 +0300

    changing learning algo

--- src/calculate/stocks/stocks_train.py
+++ src/calculate/stocks/stocks_train.py
@@ -1,5 +1,7 @@
+import joblib
 from numpy.random import seed
-from tensorflow.python.keras.optimizers import SGD
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
 
 from stocks.stocks_prepareDF import prepareDf
 
@@ -7,24 +9,32 @@ seed(1)
 from tensorflow import set_random_seed
 
 set_random_seed(2)
-import tensorflow as tf
+
+
+def report(i, y, z):
+    print(i)
 
 
 def train(test_year):
     x_train, x_test, y_train, y_test = prepareDf(test_year)
-    model = tf.keras.models.Sequential()
-    # model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.019)
-    model.compile(optimizer=sgd,
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=10)
-    val_loss, val_acc = model.evaluate(x_test, y_test)
-    print(val_acc)
-    print(val_loss)
+    model = ensemble.GradientBoostingRegressor(
+        n_estimators=500,
+        learning_rate=0.1,
+        max_depth=6,
+        min_samples_leaf=6,
+        max_features=0.2,
+        loss='huber',
+        random_state=0
+    )
+    model.fit(x_train, y_train, monitor=report)
 
-    model.save('epic_num_reader.model')
+    # Save the trained model to a file so we can use it in other programs
+    joblib.dump(model, 'trained_house_classifier_model.pkl')
+
+    # Find the error rate on the training set
+    mse = mean_absolute_error(y_train, model.predict(x_train))
+    print("Training Set Mean Absolute Error: %.4f" % mse)
+
+    # Find the error rate on the test set
+    mse = mean_absolute_error(y_test, model.predict(x_test))
+    print("Test Set Mean Absolute Error: %.4f" % mse)
